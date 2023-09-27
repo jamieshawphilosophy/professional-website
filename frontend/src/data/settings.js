f9a5050bf32f3720b143035b7ffdfce35764a162
benbourgon <benjaminbourgon@gmail.com>
2023-09-27 11:15:49 -0400

    Switch to Astro Sanity starter

---
+++ frontend/src/data/settings.js
@@ -0,0 +1,5 @@
+export default {
+    title: `Jamie Shaw - Philosopher`,
+    description: `Hi! my name is Jamie Shaw. I am a philosopher of science. I mostly think about science funding policy and Feyerabend. `,
+    url: `https://jamieshawphilosophy.netlify.app`, // No trailing slash!
+}
