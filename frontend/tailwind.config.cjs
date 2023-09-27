b43a8a566cc168ec9651d747af60e02d614ef3ac
benbourgon <benjaminbourgon@gmail.com>
2023-09-27 17:29:33 -0400

    Added Header, Footer, Hero components

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        sans: ["Rubik Variable", "Roboto Flex Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
 	},
 	plugins: ['prettier-plugin-tailwindcss', require('@tailwindcss/typography'), require("daisyui"),],
   daisyui: {
