913b29e627afbc1625d0d130e94a9d0d16129e8f
benbourgon <benjaminbourgon@gmail.com>
2023-09-27 12:21:31 -0400

    Update config files

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import sanity from 'astro-sanity';
 import tailwind from "@astrojs/tailwind";
+import {loadEnv} from "vite";
+
+const env = loadEnv('', process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-sanity-minimal-starter.netlify.app/',
+  site: 'https://jamieshawphilosophy.netlify.app/',
   // Your public domain, e.g.: https://my-site.dev/
   integrations: [sitemap(), sanity({
-    projectId: '8hj1t7km',
-    dataset: 'production',
-    apiVersion: 'v2021-03-25',
+    projectId: env.PUBLIC_SANITY_PROJECT_ID,
+    dataset: env.PUBLIC_SANITY_DATASET,
+    apiVersion: env.PUBLIC_SANITY_API_VERSION,
     useCdn: true
   }), tailwind({applyBaseStyles: true,})]
 });
