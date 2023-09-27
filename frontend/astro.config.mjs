1054242b13b67c4bb7649482731e100e0f357a00
benbourgon <benjaminbourgon@gmail.com>
2023-09-27 13:16:19 -0400

    Change commit to debuild build

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,19 +2,15 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import sanity from 'astro-sanity';
 import tailwind from "@astrojs/tailwind";
-import { loadEnv } from "vite";
 import netlify from "@astrojs/netlify/functions";
-const env = loadEnv('', process.cwd(), '');
-
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jamieshawphilosophy.netlify.app/',
-  // Your public domain, e.g.: https://my-site.dev/
+  site: 'https://jamieshawphilosophy-staging.netlify.app/',
   integrations: [sitemap(), sanity({
-    projectId: env.PUBLIC_SANITY_PROJECT_ID,
-    dataset: env.PUBLIC_SANITY_DATASET,
-    apiVersion: env.PUBLIC_SANITY_API_VERSION,
+    projectId: "homselal",
+    dataset: "production",
+    apiVersion: "2021-10-21",
     useCdn: true
   }), tailwind({
     applyBaseStyles: true
