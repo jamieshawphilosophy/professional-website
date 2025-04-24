3093d0b6a56692f63bdb31be96ef204c8f5b03c5
Ben Bourgon <benjaminbourgon@gmail.com>
2025-04-23 22:58:08 -0400

    refactor: remove unused schema types and structure files
    - Deleted block schema types: block, grid, layout, menu, tabs.
    - Removed category, cta, document, footer, header, hero, image, job, menu, menuItem, page, post, project, publication, researchInterest, section, site-settings schema types.
    - Cleaned up structure files related to blocks and content management.
    - Updated tsconfig to disable JavaScript files.

--- astro.config.ts
+++ astro.config.ts
@@ -3,80 +3,17 @@
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import sanity from "@sanity/astro";
 // Astro Configuration
-import { defineConfig, envField } from "astro/config";
-import { loadEnv } from "vite";
+import { defineConfig } from "astro/config";
 // Utility Functions
-import { coerceBoolean } from "./src/utils";
-import { parseString } from "./src/utils";
 
 // get the current mode from the NODE_ENV environment variable
-const mode = process.env.NODE_ENV || "development";
 
-// use Vite's loadEnv to load environment variables in Astro config
-const {
-  PUBLIC_SANITY_API_VERSION,
-  PUBLIC_SANITY_DATASET,
-  PUBLIC_SANITY_PROJECT_ID,
-  PUBLIC_SANITY_STUDIO_BASE_PATH,
-  PUBLIC_SANITY_USE_CDN,
-} = loadEnv(mode, process.cwd(), "");
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://jamieshawphilsophy-staging.netlify.app",
-  env: {
-    schema: {
-      PUBLIC_SANITY_PROJECT_ID: envField.string({
-        context: "client",
-        access: "public",
-      }),
-      PUBLIC_SANITY_DATASET: envField.string({
-        context: "client",
-        access: "public",
-        default: "production",
-      }),
-      PUBLIC_SANITY_API_VERSION: envField.string({
-        context: "client",
-        access: "public",
-        default: "2024-05-09",
-      }),
-      PUBLIC_SANITY_USE_CDN: envField.boolean({
-        context: "client",
-        access: "public",
-        default: false,
-      }),
-      PUBLIC_SANITY_STUDIO_BASE_PATH: envField.string({
-        context: "client",
-        access: "public",
-        default: "/admin",
-      }),
-      PUBLIC_SANITY_VISUAL_EDITING_ENABLED: envField.boolean({
-        context: "client",
-        access: "public",
-        default: true,
-      }),
-      SANITY_API_READ_TOKEN: envField.string({
-        context: "server",
-        access: "secret",
-      }),
-    },
-  },
   // Astro Integrations
-  integrations: [
-    sitemap(),
-    sanity({
-      projectId: parseString(PUBLIC_SANITY_PROJECT_ID),
-      dataset: parseString(PUBLIC_SANITY_DATASET),
-      apiVersion: parseString(PUBLIC_SANITY_API_VERSION),
-      useCdn: coerceBoolean(PUBLIC_SANITY_USE_CDN),
-      studioBasePath: parseString(PUBLIC_SANITY_STUDIO_BASE_PATH),
-      stega: {
-        studioUrl: parseString(PUBLIC_SANITY_STUDIO_BASE_PATH),
-      },
-    }),
-    react(),
-  ],
+  integrations: [sitemap(), react()],
   adapter: netlify(),
 });
