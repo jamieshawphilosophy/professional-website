d4547df1f69234dba7d1215090aa003dc9bac1a0
Ben Bourgon <benjaminbourgon@gmail.com>
2025-05-10 20:48:13 -0400

    Remove template React components, begin setting up project structure

---
+++ src/blog/post1.md
@@ -0,0 +1,14 @@
+---
+author: string
+pubDatetime: date
+modDatetime: date
+title: string
+ogImage: string
+slug: string
+featured: boolean
+draft: boolean
+tags: boolean
+description: string
+body: rich-text
+---
+
---
+++ src/types.ts
@@ -0,0 +1,19 @@
+import type socialIcons from "@src/images/socialIcons/socialIcons.ts";
+
+export type Site = {
+  website: string;
+  author: string;
+  desc: string;
+  title: string;
+  ogImage?: string;
+  lightAndDarkMode: boolean;
+  postPerPage: number;
+  scheduledPostMargin: number;
+};
+
+export type SocialObjects = {
+  name: keyof typeof socialIcons;
+  href: string;
+  active: boolean;
+  linkTitle: string;
+}[];
