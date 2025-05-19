8660044018d91e6da5c08e57af49ef99e76e20c3
Ben Bourgon <benjaminbourgon@gmail.com>
2025-05-19 13:20:49 -0400

    fix: update dependencies and restructure Head component

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,10 @@
 // astro.config.ts
 // Astro Integrations
-import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 // Astro Configuration
 import { defineConfig } from "astro/config";
 import { SITE } from "./src/config";
-// Utility Functions
-
-// get the current mode from the NODE_ENV environment variable
-
 // https://astro.build/config
 export default defineConfig({
   output: "static",
